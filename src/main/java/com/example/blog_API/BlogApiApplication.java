2ca68ad8466343e948273ef178954f510426c377
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2024-09-07 19:30:26 -0300

    inital project structure

---
+++ src/main/java/com/example/blog_API/BlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.blog_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApiApplication.class, args);
+	}
+
+}
