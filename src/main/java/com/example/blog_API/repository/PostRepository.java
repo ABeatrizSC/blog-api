b9e0c0acd656fedae88ada7ac501c7abce3ef6f3
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2024-09-09 13:38:10 -0300

    feat: simple query with @query

--- src/main/java/com/example/blog_API/repository/PostRepository.java
+++ src/main/java/com/example/blog_API/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.example.blog_API.repository;
 
 import com.example.blog_API.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,7 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 }
